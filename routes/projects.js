1870ab7050dcddfdee634c45d76720915b791b00
Ross Keeley <rjkeeley@gmail.com>
2021-02-01 16:25:28 +0000

    Changed route to render projects and users pages to template index.ejs

--- routes/projects.js
+++ routes/projects.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var jsonFile = require('../data/projects.json');
 var projectsData = require('../data/projects.json');
 
 
 router.get('/', function(req, res, next) {
-  res.render('pages/projects', {projectsData} );
+  res.render('index', { template: 'projects', projectsData });
 });
 
 module.exports = router;
