2cd684da904b25ec0a98230ec80da717d22ab447
Ross Keeley <rjkeeley@gmail.com>
2021-02-01 11:31:00 +0000

    Added images to json file and rendered on projects page

--- routes/projects.js
+++ routes/projects.js
@@ -1,56 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var jsonFile = require('../data/projects.json');
-var projectsData =
-[
-    {
-      "title": "Calculator App",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      "image": "calculator.jpeg",
-      "tags": [
-        "HTML",
-        "CSS",
-        "JavaScript",
-        "ReactJS"
-      ]
-    },
-    {
-      "title": "Markdown Previewer",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      "tags": [
-        "HTML",
-        "CSS",
-        "JavaScript"
-      ]
-    },
-    {
-      "title": "Random Quote Generator",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      "tags": [
-        "HTML",
-        "CSS",
-        "JavaScript"
-      ]
-    },
-    {
-      "title": "Pomodoro Clock",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      "tags": [
-        "HTML",
-        "CSS",
-        "JavaScript"
-      ]
-    },
-    {
-      "title": "Drum Machine",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      "tags": [
-        "HTML",
-        "CSS",
-        "JavaScript"
-      ]
-    }
-  ];
+var projectsData = require('../data/projects.json');
+
 
 router.get('/', function(req, res, next) {
   res.render('pages/projects', {projectsData} );
