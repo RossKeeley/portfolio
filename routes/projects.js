8cb5ac991309c2517bfe18184e78256e4c03997e
RossKeeley <67203345+RossKeeley@users.noreply.github.com>
2021-02-04 12:18:42 +0000

    Added projectId nested route to projects

--- routes/projects.js
+++ routes/projects.js
@@ -7,4 +7,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { pageTitle: 'Projects | Ross Keeley Portfolio', style: 'main', template: 'projects', projectsData, javascript: 'main' });
 });
 
+router.get('/:projectId', (req, res) => {
+  let projectName = req.params.projectId;
+  res.render('index', { pageTitle: projectName + ' | Ross Keeley Portfolio', style: projectName, template: projectName, javascript: projectName });
+});
+
 module.exports = router;
