a26d5faa80cdfa107dacc9140f14e3828d88c722
Ross Keeley <rjkeeley@gmail.com>
2021-02-07 11:21:37 +0000

    Updated js and css file structure

--- public/javascripts/markdown-previewer.js
+++ public/javascripts/markdown-previewer.js
@@ -1,27 +1,17 @@
-<script type="text/javascript">
-  // Header JS
-  window.addEventListener('scroll', () => {
-    var header = document.querySelector('header');
-    header.classList.toggle('sticky', window.scrollY > 0);
-  });
+const exampleText = "";
 
-  // Markdown Previewer JS
-  const exampleText = "";
+marked.setOptions({breaks: true});
+var input = document.getElementById("editor");
+var preview = document.getElementById("preview");
 
-  marked.setOptions({breaks: true});
-  var input = document.getElementById("editor");
-  var preview = document.getElementById("preview");
+preview.innerHTML = marked(input.value);
 
+input.addEventListener("keyup", () => {
   preview.innerHTML = marked(input.value);
+});
 
-  input.addEventListener("keyup", () => {
-    preview.innerHTML = marked(input.value);
-  });
-
-  // Clear button
-  document.getElementById("clearButton").addEventListener("click", () => {
-    input.value = "";
-    preview.innerHTML = "";
-  });
-</script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/marked/1.2.7/marked.min.js"></script>
+// Clear button
+document.getElementById("clearButton").addEventListener("click", () => {
+  input.value = "";
+  preview.innerHTML = "";
+});
