d71be828640dcb25b29d1c9988652b725d7ec8fd
Ross Keeley <rjkeeley@gmail.com>
2021-02-16 17:02:09 +0000

    Made homepage responsive

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,3 +2,12 @@ window.addEventListener('scroll', () => {
   var header = document.querySelector('header');
   header.classList.toggle('sticky', window.scrollY > 0);
 });
+
+function myFunction() {
+  var x = document.getElementById("menu");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
