9b67f00ce15d77f2a89c532610f622390cd5cf77
Ross Keeley <rjkeeley@gmail.com>
2021-02-17 12:23:15 +0000

    Added responsive burger menu to header

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,10 +1,20 @@
-window.addEventListener('scroll', () => {
-  var header = document.querySelector('header');
-  header.classList.toggle('sticky', window.scrollY > 0);
+// window.addEventListener('scroll', () => {
+//   var header = document.querySelector('header');
+//   header.classList.toggle('sticky', window.scrollY > 0);
+// });
+
+window.addEventListener("resize", () => {
+  var width = screen.width;
+  var x = document.getElementById("item");
+  if (width < 670) {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  };
 });
 
 function myFunction() {
-  var x = document.getElementById("menu");
+  var x = document.getElementById("item");
   if (x.style.display === "block") {
     x.style.display = "none";
   } else {
